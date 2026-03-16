fcf7a1f76c96d95d81d41e09a46c361e26f02ff9
Hayden Feddock <hayden4feddock@gmail.com>
2026-03-16 11:55:21 -0400

    Updated default args bringup.

--- erwinia_os_bringup/launch/bringup.launch.py
+++ erwinia_os_bringup/launch/bringup.launch.py
@@ -160,12 +160,12 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_gazebo',
-            default_value='true',
+            default_value='false',
             description='Whether to use Gazebo simulation'
         ),
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             description='Use simulation time'
         ),
         DeclareLaunchArgument(
